90cada0cd1c8742d7c12dd108e82f40755d3aad1
Audrey Akwenye <audrey@schoolio.co>
2019-04-23 17:55:17 -0500

    better

--- cal/models.py
+++ cal/models.py
@@ -6,3 +6,6 @@ class Event(models.Model):
     category = models.CharField(max_length=30, default='main')
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
