d130eed184ec096e09b6d768bd847fe2d87178c4
Audrey Akwenye <audrey@schoolio.co>
2019-05-02 02:04:42 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ PAYPAL_TEST = True
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
