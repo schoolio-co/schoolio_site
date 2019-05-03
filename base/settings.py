5dcc8e2bc5a263cebaf5507c3e03011f9366c331
Audrey Akwenye <audrey@schoolio.co>
2019-05-02 20:15:46 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -137,6 +137,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
