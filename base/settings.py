fe67adbc51300311dca1a94fe8e0b42b822fe0f8
Audrey Akwenye <audrey@schoolio.co>
2019-05-03 23:32:59 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -143,5 +143,6 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL='/login/'
+LOGIN_REDIRECT_URL='/login/'
 django_heroku.settings(locals())
