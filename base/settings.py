b09b5004a4d70372d7e745110de39237a897b98e
Audrey Akwenye <audrey@schoolio.co>
2019-05-05 20:30:17 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -16,9 +16,9 @@ from whitenoise import WhiteNoise
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 
-PAYPAL_RECEIVER_EMAIL = 'audrey-facilitator@schoolio.co'
+PAYPAL_RECEIVER_EMAIL = 'audrey@schoolio.co'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
-PAYPAL_TEST = True
+PAYPAL_TEST = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
