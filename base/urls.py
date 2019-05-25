535dcf576522b2f6c30692a8ed4621fca2903255
Audrey Akwenye <audrey@schoolio.co>
2019-05-24 23:14:19 -0500

    better

--- base/urls.py
+++ base/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('quiz.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 if settings.DEBUG: # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
