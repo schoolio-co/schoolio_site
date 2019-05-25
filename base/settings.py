64a4ad74553026c5ac03dca7e52f2fd3ac90bdb3
Audrey Akwenye <audrey@schoolio.co>
2019-05-25 17:44:20 -0500

    aws config

--- base/settings.py
+++ base/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 from whitenoise import WhiteNoise
+from base.aws.conf import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 
