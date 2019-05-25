6d1e872a0c8c29be0c6b38a52482cde9215ea629
Audrey Akwenye <audrey@schoolio.co>
2019-05-24 22:55:58 -0500

    s3

--- base/settings.py
+++ base/settings.py
@@ -31,13 +31,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['schoolioco.herokuapp.com']
 
-STATICFILES_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
-
-AWS_ACCESS_KEY_ID = config(“AWS_ACCESS_KEY_ID”)
-AWS_SECRET_ACCESS_KEY = config(“AWS_SECRET_ACCESS_KEY”)
-AWS_STORAGE_BUCKET_NAME = config(“AWS_STORAGE_BUCKET_NAME”)
-AWS_QUERYSTRING_AUTH = False 
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com’
 
 
 # Application definition
@@ -148,15 +141,26 @@ SECURE_SSL_REDIRECT = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
-MEDIA_URL = STATIC_URL + ‘media/’
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, “static”), )
-STATIC_ROOT = ‘staticfiles’
-ADMIN_MEDIA_PREFIX = STATIC_URL + ‘admin/’
-STATICFILES_FINDERS = (
-‘django.contrib.staticfiles.finders.FileSystemFinder’,
-‘django.contrib.staticfiles.finders.AppDirectoriesFinder’,
+
+
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID = config(“AWS_ACCESS_KEY_ID”)
+AWS_SECRET_ACCESS_KEY = config(“AWS_SECRET_ACCESS_KEY”)
+AWS_STORAGE_BUCKET_NAME = config(“AWS_STORAGE_BUCKET_NAME”)
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {    
+     'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] 
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+AWS_DEFAULT_ACL = None
 
 LOGIN_URL='/login/'
 LOGIN_REDIRECT_URL='/login/'
