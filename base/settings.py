b2e475143558c9c77f72d077a77b4c8f9fbd5293
audreyakwenye <audrey@schoolio.co>
2019-09-03 13:41:39 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config, Csv
 from whitenoise import WhiteNoise
-from base.aws.conf import *
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
