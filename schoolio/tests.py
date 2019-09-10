3aa21ec55a51d51a7f9505d0873c6bd35164e105
Dustan Levenstein <dustanl@math.ucla.edu>
2019-09-10 16:08:39 -0500

    Added test from dev.

--- schoolio/tests.py
+++ schoolio/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from schoolio.models import *
 
-# Create your tests here.
+
+class SchoolTest(TestCase):
+    def setUp(self):
+        school.objects.create(name="anderson", address = "anderson")
+
+    def test_thing(self):
+        """"""
+        anderson = school.objects.get(name="anderson")
+        self.assertEqual(str(anderson), 'anderson')
+
