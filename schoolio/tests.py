591c337b779a2fa613511f0796c2120ca0544616
Dustan Levenstein <dustanl@math.ucla.edu>
2019-09-10 16:18:12 -0500

    Added skeleton of a test.

--- schoolio/tests.py
+++ schoolio/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from schoolio.models import *
-
+from schoolio.standard_matching import match_standard, match_activity
 
 class SchoolTest(TestCase):
     def setUp(self):
@@ -9,5 +9,8 @@ class SchoolTest(TestCase):
     def test_thing(self):
         """"""
         anderson = school.objects.get(name="anderson")
-        self.assertEqual(str(anderson), 'anderson')
+        self.assertEqual(anderson.name, 'anderson')
 
+class MatchTest(TestCase):
+    def setUp(self):
+        pass
