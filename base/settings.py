4db20872d0c7398e8720fff450e08b9d57308222
audreyakwenye <audrey@schoolio.co>
2019-09-16 15:22:08 -0500

    better

--- base/settings.py
+++ base/settings.py
@@ -267,7 +267,7 @@ AWS_DEFAULT_ACL='public-read'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = 'https://%s/%s/static' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
