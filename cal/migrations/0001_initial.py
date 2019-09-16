b9b745d4ac11a3079c2fcebfb5c0ad209f697662
audreyakwenye <audrey@schoolio.co>
2019-09-15 22:20:52 -0500

    better

--- cal/migrations/0001_initial.py
+++ cal/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
-                ('category', models.CharField(default='main', max_length=30)),
+                ('category', models.CharField(max_length=30)),
                 ('start_time', models.DateTimeField()),
                 ('end_time', models.DateTimeField()),
                 ('school', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='schoolio.school')),
