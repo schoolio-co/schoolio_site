852f8ea5c0070194d9aba5d8cb973a6943ca5ec1
audreyakwenye <audrey@schoolio.co>
2019-09-16 21:41:00 -0500

    better

--- essay/migrations/0002_auto_20190917_0223.py
+++ essay/migrations/0002_auto_20190917_0223.py
@@ -9,21 +9,25 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('essay', '0001_initial'),
         ('quiz', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='TF_Question',
+            name='Essay_Question',
             fields=[
                 ('question_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='quiz.Question')),
-                ('correct', models.BooleanField(default=False, help_text='Tick this if the question is true. Leave it blank for false.', verbose_name='Correct')),
             ],
             options={
-                'verbose_name': 'True/False Question',
-                'verbose_name_plural': 'True/False Questions',
-                'ordering': ['category'],
+                'verbose_name': 'Essay style question',
+                'verbose_name_plural': 'Essay style questions',
             },
             bases=('quiz.question',),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='question',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='essay.Essay_Question', verbose_name='Question'),
         ),
     ]
