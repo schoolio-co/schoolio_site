aa72bca0cb39a289de6cce73b1092e03fba7c837
audreyakwenye <audrey@schoolio.co>
2019-09-26 13:25:36 -0500

    better

--- schoolio/admin.py
+++ schoolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import school, User, student_profiles, standards, day_of_the_week, classroom, lesson_school_info,  subjects, activities, classroom_subject_summary
+from .models import school, User, TeacherSchedule, student_profiles, standards, day_of_the_week, classroom, lesson_school_info,  subjects, activities, classroom_subject_summary
 
 admin.site.register(school)
 admin.site.register(standards)
@@ -11,4 +11,5 @@ admin.site.register(student_profiles)
 admin.site.register(activities)
 admin.site.register(classroom)
 admin.site.register(classroom_subject_summary)
+admin.site.register(TeacherSchedule)
 
